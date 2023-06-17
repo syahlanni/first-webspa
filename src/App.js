d5bd591123ed4436b1b97598e0abf9ddc6c7fdec
syahlanni <120327147+syahlanni@users.noreply.github.com>
2023-06-17 21:20:23 +0700

    hi

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Routes, NavLink, HashRouter } from "react-router-dom";
+import './style.css';
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1 className="title">Single Page Application</h1>
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Routes exact path="/" component={Home} />
+            <Routes path="/about" component={About} />
+            <Routes path="/contact" component={Contact} />
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+export default Main;
